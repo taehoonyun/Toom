0b768c528da5b249e498507c5fb8b92985897a6e
taehoonyun <mn9069@naver.com>
2022-09-14 06:46:27 +0900

    socke.io server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
-alert("hi");
+const socket = io();
+
+const welcome = document.querySelector("#welcome");
+const form = document.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,14 +1,56 @@
 import express from "express";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => res.render("home"));
 app.use("/public", express.static(__dirname + "/public"));
 
-const hadleListen = () => {
-  console.log("working!");
-};
-app.listen(3000, hadleListen);
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+httpServer.listen(3000, () => {
+  console.log("Server is working");
+});
+
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
+});
+
+// const server = http.createServer(app);
+
+// const wss = new WebSocketServer({ server });
+
+// const sokets = [];
+
+// wss.on("connection", (socket) => {
+//   sokets.push(socket);
+//   console.log("connected to Browser");
+//   socket.on("message", (message) => {
+//     sokets.forEach((aSocket) => aSocket.send(message));
+//   });
+// });
+
+// server.listen(3000, handleListen);
+
+// {
+//   type: "message";
+//   payload: "hello everyone!";
+// }
+
+// {
+//   type: "nickname";
+//   payload: "tae";
+// }
